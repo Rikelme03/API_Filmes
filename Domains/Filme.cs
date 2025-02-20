43edefe0232998400ebb55028ab990e1240ec721
RIKELME MIRANDA SILVA <47540013893@info.local>
2025-02-20 09:00:19 -0300

    API_Filmes

---
+++ Domains/Filme.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_filmes_senai.Domains
+{
+    [Table("Filme")]
+
+    public class Filme
+    {
+        [Key]
+        public Guid IdFilme { get; set; }
+
+        [Column(TypeName = "VARCHAR(50)")]
+
+        [Required(ErrorMessage = "O Titulo do filme obrigatorio! ")]
+
+        public string? Titulo { get; set; }
+
+
+        /// <summary>
+        /// Referencia da tabela Genero
+        /// </summary>
+
+        public Guid IdGenero { get; set; }
+
+        [ForeignKey("IdGenero")]
+
+        public Genero? Genero { get; set; }
+
+    }
+}
---
+++ Domains/Genero.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_filmes_senai.Domains
+{   
+    [Table("Genero")]
+
+    public class Genero
+    {
+        
+
+        [Key]
+        public Guid IdGenero { get; set; }
+
+        [Column(TypeName = "VARCHAR(30)")]
+
+        [Required(ErrorMessage = "Nome do Genero obrigatorio !")]
+
+        public string? Nome { get; set; }
+
+    }
+}
